d18f194426717154c7b5367da8e7c71a59650b0a
Yacho-glitch <181276632+Yacho-glitch@users.noreply.github.com>
2025-06-23 01:23:25 +0100

    Apend sql page to the project

---
+++ sql.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE school;
+USE school;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    age INT NOT NULL,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
